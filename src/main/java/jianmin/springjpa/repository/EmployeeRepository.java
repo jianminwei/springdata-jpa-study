5d5e8475a727afe01a53fe2ce5276869d2d8dcb6
Jianmin Wei <jianminwei1@gmail.com>
2022-05-16 06:54:00 -0400

    Study08 - added @EntityGraph(attributePaths = department) for findAll() EmployeeRepo. Now the findEmployeeDept() run just 1 query

--- src/main/java/jianmin/springjpa/repository/EmployeeRepository.java
+++ src/main/java/jianmin/springjpa/repository/EmployeeRepository.java
@@ -1,8 +1,23 @@
 package jianmin.springjpa.repository;
 
 import jianmin.springjpa.model.Employee;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface EmployeeRepository extends JpaRepository<Employee, Long> {
+
+    /**
+     * By default, Hibernate use "LAZY" fetching for the ManyToOne Employee to Deportment
+     * relationship. That means each time when you get Department data from Employee object,
+     * you will incur a SQL query.
+     * To avoid that, you can tell Hibernate to do a join with Department using @EntityGraph
+     *
+     * @return List<Employee>
+     */
+    @Override
+    @EntityGraph(attributePaths = "department")
+    List<Employee> findAll();
 
 }
